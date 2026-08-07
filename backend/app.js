6c555a3cb1697af13f7229650b2cffb928baf2f8
ellen.kuo <ellen.kuo@patrioimemory.com>
2026-08-07 15:59:20 +0800

    feat: 修改專案入口設定

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const cors = require("cors")
+const { dataSource } = require("./db/data-source")
 const app = express()
 require("dotenv").config()
 
@@ -7,11 +8,15 @@ const { healthcheck } = require("./controllers/healthcheck")
 const routes = require("./routes")
 const PORT = Number(process.env.PORT)
 
-
 app.use(cors())
 app.use(express.json())
 app.use(routes)
-
-app.listen(PORT, () => {
-  console.log(`app is listening on http://localhost:${PORT}`)
+app.use((req, res) => {
+  res.status(404).json({status:"error", message:"路由不存在"})
 })
+app.use((err, req, res, next) => {
+  console.error(err)
+  res.status(500).json({status:"error", err:err.name, message:err.message})
+})
+
+module.exports = app
