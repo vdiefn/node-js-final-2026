fb6d9076f441797637a139bfa7722751086df29a
ellen.kuo <ellen.kuo@patrioimemory.com>
2026-08-18 15:56:30 +0800

    feat: 更新data-source設定

--- backend/db/data-source.js
+++ backend/db/data-source.js
@@ -16,7 +16,7 @@ const dataSource = new DataSource({
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
-  synchronize: false,
+  synchronize: process.env.DB_SYNCHRONIZE,
   entities: [Skill, CreditPackage, User, Coach, CoachSkill, Course, CreditPurchase, CourseBooking],
   migrations: ["db/migrations/*.js"],
 });
